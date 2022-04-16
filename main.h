bb6fecd15d66ddf94198ce52e1b4e4dfabc285c6
mongarerobert3 <mongarerobert3@gmail.com>
2022-04-16 03:50:09 -0700

    function that produces output according to a format

--- main.h
+++ main.h
@@ -1,6 +1,23 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
-int _putchar (char c);
+
+/**
+ * struct specifier - struct specifier
+ * @valid: the valid character.
+ * @f: the functions associated.
+ *
+ */
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} spec;
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int _putchar(char c);
 int _printf(const char *format, ...);
+
 #endif
